2aaf8964601bce6d4a8913341fed368607794ccd
lzxm160 <lzxm160@sina.com>
2017-02-17 10:48:49 +0800

    init

--- src/mainproject/pdf.go
+++ src/mainproject/pdf.go
@@ -1,10 +1,8 @@
  package main
  import (
-    "log"
+    _"log"
     "fmt"
-    "time"
-    "strconv"
-    "go_redis_cluster"
+    
     "net/http"
 )
 func pdfHandler (w http.ResponseWriter, r *http.Request) {
