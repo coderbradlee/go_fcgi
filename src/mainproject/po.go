4e05cff2a05efb49a94e6fba0e93b0e656a2387c
lzxm160 <lzxm160@sina.com>
2017-02-20 17:09:03 +0800

    init

--- src/mainproject/po.go
+++ src/mainproject/po.go
@@ -173,7 +173,7 @@ func poHandler (w http.ResponseWriter, r *http.Request) {
 	    }
 	    
 	    fmt.Printf("response: %s\n", buffer.Bytes())
-	    fmt.Fprint(w, buffer.Bytes())
+	    fmt.Fprint(w, buffer.Bytes.String())
 	    // 
 	    // 
 	    ////////////////////////////////////
