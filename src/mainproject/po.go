1aa5e1aceb6a1d62f002a4253a443481750c9fe5
lzxm160 <lzxm160@sina.com>
2017-02-20 17:27:42 +0800

    init

--- src/mainproject/po.go
+++ src/mainproject/po.go
@@ -8,101 +8,101 @@
     "bytes"
 )
 type Detail struct{
-	product_name string
-	product_code string
-	item_no string
-	unit_price float64
-	quantity int32
-	uom string
-	sub_total float64
-	warranty int32
-	comments string
-	note string
+	Product_name string `json:"product_name"`
+	Product_code string `json:"product_code"`
+	Item_no string `json:"item_no"`
+	Unit_price float64 `json:"unit_price"`
+	Quantity int32 `json:"quantity"`
+	Uom string `json:"uom"`
+	Sub_total float64 `json:"sub_total"`
+	Warranty int32 `json:"warranty"`
+	Comments string `json:"comments"`
+	Note string `json:"note"`
 }
 type Purchase_order struct{
-	bill_type string
-	po_no string
-	po_url string
-	po_date string
-	create_by string
-	status int32
-	supplier string
-	website string
-	company string
-	requested_delivery_date string
-	trade_term  string
-	payment_terms string
-	ship_via string
-	destination_country string
-	loading_port string
-	certificate string
-	total_quantity int32
-	total_amount float64
-	currency string
-	comments string
-	note string
-	detail []Detail
+	Bill_type string `json:"bill_type"`
+	Po_no string `json:"po_no"`
+	Po_url string `json:"po_url"`
+	Po_date string `json:"po_date"`
+	Create_by string `json:"create_by"`
+	Status int32 `json:"status"`
+	Supplier string `json:"supplier"`
+	Website string `json:"website"`
+	Company string `json:"company"`
+	Requested_delivery_date string `json:"requested_delivery_date"`
+	Trade_term  string `json:"trade_term"`
+	Payment_terms string `json:"payment_terms"`
+	Ship_via string `json:"ship_via"`
+	Destination_country string `json:"destination_country"`
+	Loading_port string `json:"loading_port"`
+	Certificate string `json:"certificate"`
+	Total_quantity int32 `json:"total_quantity"`
+	Total_amount float64 `json:"total_amount"`
+	Currency string `json:"total_amount"`
+	Comments string `json:"comments"`
+	Note string `json:"note"`
+	Detail []Detail `json:"detail"`
 }
 type Commercial_invoice struct{
-	ci_no string
-	ci_url string
+	Ci_no string `json:"ci_no"`
+	Ci_url string `json:"ci_url"`
 }
 type Packing_list struct{
-	pl_no string
-	pl_url string
+	Pl_no string `json:"pl_no"`
+	Pl_url string `json:"pl_url"`
 }
 type Bill_of_lading struct{
-	bl_no string
-	bl_url string
+	Bl_no string `json:"bl_no"`
+	Bl_url string `json:"bl_url"`
 }
 type Associated_so struct{
-	associated_so_no string
-	associated_so_url string
+	Associated_so_no string `json:"associated_so_no"`
+	Associated_so_url string `json:"associated_so_url"`
 }
 
 type Deliver_notes_detail struct{
-	product_name string
-	product_code string
-	item_no string
-	unit_price float64
-	quantity int32
-	uom string
-	sub_total float64
+	Product_name string `json:"product_name"`
+	Product_code string `json:"product_code"`
+	Item_no string `json:"item_no"`
+	Unit_price float64 `json:"unit_price"`
+	Quantity int32 `json:"quantity"`
+	Uom string `json:"uom"`
+	Sub_total float64 `json:"sub_total"`
 }
 type Deliver_notes struct{
-	supplier string
-	buyer string
-	loading_port string
-	trade_term string
-	ship_via string
-	packing_method string
-	logistic string
-	logistic_contact string
-	logistic_contact_email string
-	logistic_contact_telephone_number string
-	etd string
-	eta string
-	customs_clearance_date string
-	total_freight_charges float64
-	total_insurance_fee float64
-	total_excluded_tax float64
-	currency string
-	commercial_invoice Commercial_invoice
-	packing_list Packing_list
-	bill_of_lading Bill_of_lading
-	associated_so Associated_so
-	detail []Deliver_notes_detail
+	Supplier string `json:"supplier"`
+	Buyer string `json:"buyer"`
+	Loading_port string `json:"loading_port"`
+	Trade_term string `json:"trade_term"`
+	Ship_via string `json:"ship_via"`
+	Packing_method string `json:"packing_method"`
+	Logistic string `json:"logistic"`
+	Logistic_contact string `json:"logistic_contact"`
+	Logistic_contact_email string `json:"logistic_contact_email"`
+	Logistic_contact_telephone_number string `json:"logistic_contact_telephone_number"`
+	Etd string `json:"etd"`
+	Eta string `json:"eta"`
+	Customs_clearance_date string `json:"customs_clearance_date"`
+	Total_freight_charges float64 `json:"total_freight_charges"`
+	Total_insurance_fee float64 `json:"total_insurance_fee"`
+	Total_excluded_tax float64 `json:"total_excluded_tax"`
+	Currency string `json:"currency"`
+	Commercial_invoice Commercial_invoice `json:"commercial_invoice"`
+	Packing_list Packing_list `json:"packing_list"`
+	Bill_of_lading Bill_of_lading `json:"bill_of_lading"`
+	Associated_so Associated_so `json:"associated_so"`
+	Detail []Deliver_notes_detail `json:"detail"`
 
 }
-type Data struct{
-	request_system int32
-	request_time string
-	purchase_order Purchase_order
-	deliver_notes []Deliver_notes
+type Datas struct{
+	Request_system int32 `json:"request_system"`
+	Request_time string `json:"request_time"`
+	Purchase_order Purchase_order `json:"purchase_order"`
+	Deliver_notes []Deliver_notes `json:"deliver_notes"`
 }
 type DeliverGoodsForPO struct {
-   operation string
-   data Data 
+   Operation string `json:"operation"`
+   Data Datas  `json:"data"`
 }
 type Response_json_data struct{
 	Goods_receipt_no string `json:"goods_receipt_no"`
