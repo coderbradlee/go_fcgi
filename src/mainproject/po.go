590a217ba857822eac8f1bfeabec668e8d5d915a
lzxm160 <lzxm160@sina.com>
2017-02-20 17:04:56 +0800

    init

--- src/mainproject/po.go
+++ src/mainproject/po.go
@@ -5,8 +5,7 @@
     "encoding/json"
     "net/http"
     _"io/ioutil"
-    _"bytes"
-    "os"
+    "bytes"
 )
 type Detail struct{
 	product_name string
@@ -115,7 +114,7 @@ type Response_json_data struct{
 type Response_json struct{
 	Error_code string
 	Error_msg string
-	Data Response_json_Data	
+	Data Response_json_data	
 	Reply_time string		   
 }
 func poHandler (w http.ResponseWriter, r *http.Request) {
@@ -153,7 +152,7 @@ func poHandler (w http.ResponseWriter, r *http.Request) {
 	    log.Println(t.operation)
 
 	    // response_data:=Response_json_data{goods_receipt_no:"GR-FR-20170226-000196",bill_type:"Goods Receipt",receive_by:"Enie Yang",company:"ReneSola France",receive_at:"2017-03-17 12:00:00"}
-	    json_ret:=&Response_json{error_code:"200",error_msg:"Goods received successfully at 2017-03-17 12:00:00",data:Response_json_data{goods_receipt_no:"GR-FR-20170226-000196",bill_type:"Goods Receipt",receive_by:"Enie Yang",company:"ReneSola France",receive_at:"2017-03-17 12:00:00"},reply_time:"2017-03-17 12:00:00"}
+	    json_ret:=&Response_json{Error_code:"200",Error_msg:"Goods received successfully at 2017-03-17 12:00:00",Data:Response_json_data{Goods_receipt_no:"GR-FR-20170226-000196",Bill_type:"Goods Receipt",Receive_by:"Enie Yang",Company:"ReneSola France",Receive_at:"2017-03-17 12:00:00"},Ceply_time:"2017-03-17 12:00:00"}
 		// fmt.Println(json_ret)
 	    // encoder:=json.NewEncoder(w)
 	    // err_encode:=encoder.Encode(json_ret)
