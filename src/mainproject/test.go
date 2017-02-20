308968554ef81b456688b58a3972261af2440f19
lzxm160 <lzxm160@sina.com>
2017-02-21 06:53:15 +0800

    database

---
+++ src/mainproject/test.go
@@ -0,0 +1,38 @@
+import(
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+)
+var palette=[]color.Color{color.White,color.Black}
+const(
+	whiteIndex=0
+	blackIndex=1
+)
+func main() {
+	lissajous(os.Stdout)
+}
+func lissajous(out io.Writer) {
+	const(
+		cycles=5
+		res=0.001
+		size=100
+		nframes=64
+		delay=10
+	)
+	freq:=rand.Float64()*3.0
+	anim:=gif.Gif{LoopCount:nframes}
+	phase:=0.0
+	for i:=0;i<nframes;i++{
+		rect:=image.Rect(0,0,2*size+1,2*size+1)
+		img:=image.NewPalletted(rect,palette)
+		for t:=0.0;t<cycles*2*math.Pi;t+=res{
+			x:=math.Sin(t)
+			y:=math.Sin(t*freq+phase)
+			img.SetColorIndex(size+int(x*size+0.5),size+int(y*size+0.5))
+		}
+	}
+}
