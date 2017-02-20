08e982fd8541bd1fb8f52a8aea2ef030a6438437
lzxm160 <lzxm160@sina.com>
2017-02-20 21:53:23 +0800

    database

--- src/mainproject/main.go
+++ src/mainproject/main.go
@@ -13,16 +13,25 @@ import (
     "runtime" 
     "net" 
     "net/http/fcgi"
-    "martini")
+    "martini"
+    "database/sql")
+type mysql_conf struct{
+    Host string
+    Port string
+    Username string
+    Password string
+    Database string
+}
 type Configuration struct {
     Exec_time string
     FastcgiPort string
     Log_name string
     HttpPort string
     RedisNodes []string
+    Mysql_conf mysql_conf
 }
 var configuration Configuration
-
+var db *sql.DB
 func init() {
     runtime.GOMAXPROCS(runtime.NumCPU())
     file, _ := os.Open("src/mainproject/conf.json")
@@ -36,8 +45,17 @@ func init() {
     fmt.Printf("FastcgiPort %s\n",configuration.FastcgiPort)
     fmt.Printf("Log_name %s\n",configuration.Log_name)
     fmt.Printf("HttpPort %s\n",configuration.HttpPort)
+    mysql_init()
     log_init()
     //fmt.Println(configuration.exec_time) // output: [UserA, UserB]
+}
+func mysql_init() {
+    conn_string:=configuration.Mysql_conf.Username+":"+configuration.Mysql_conf.Password+"@tcp("+configuration.Mysql_conf.Host+":"+configuration.Mysql_conf.Port+")"+configuration.Mysql_conf.Database+"?charset=utf8"
+    // db, _ = sql.Open("mysql", "renesola:renes0la.xx@tcp(172.18.22.202:3306)/apollo_eu_erp?charset=utf8")
+    db, _ = sql.Open("mysql", conn_string)
+    db.SetMaxOpenConns(20)
+    db.SetMaxIdleConns(10)
+    db.Ping()
 }
 func log_init() {
     log_name:=fmt.Sprintf("%s",configuration.Log_name)
