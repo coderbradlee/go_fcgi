b80757045dbfe5edf3e660865f6318e233c2c530
lzxm160 <lzxm160@sina.com>
2017-02-21 14:59:18 +0800

    init

--- src/mainproject/purchase_order.go
+++ src/mainproject/purchase_order.go
@@ -10,3 +10,11 @@ func get_company_id(company string) string{
 
     return company_id
 }
+func get_shipping_method_id(Ship_via string) string{
+	//cannot find the way to shipping_method_id
+	// var shipping_method_id string
+ //    db.QueryRow("select shipping_method_id from t_company where short_name=?",company).Scan(&shipping_method_id)
+	var shipping_via_id string
+    db.QueryRow("select shipping_via_id from t_ship_via where full_name=?",Ship_via).Scan(&shipping_via_id)
+    return shipping_via_id
+}
