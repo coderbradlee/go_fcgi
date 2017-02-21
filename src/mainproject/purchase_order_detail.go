9faa770618a0f846449520d3f625403915e35db1
lzxm160 <lzxm160@sina.com>
2017-02-21 14:00:30 +0800

    init

--- src/mainproject/purchase_order_detail.go
+++ src/mainproject/purchase_order_detail.go
@@ -4,7 +4,7 @@
     "time"
 )
 func insert_purchase_order_detail(t *purchase_order,origi *DeliverGoodsForPO)error {
-	for index,detail:= range origi.Data.Purchase_order.detail{
+	for index,detail:= range origi.Data.Purchase_order.Detail{
 		_, err := db.Exec(
         `INSERT INTO t_purchase_order_detail(detail_id,purchase_order_id,
 		item_master_id,unit_price,quantity,uom_id,sub_amount,warranty,
