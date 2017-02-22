b7b985b71b94c72be7728a0c855918bea27d50c0
lzxm160 <lzxm160@sina.com>
2017-02-22 14:00:39 +0800

    init

--- src/mainproject/check_data.go
+++ src/mainproject/check_data.go
@@ -8,10 +8,17 @@ const(
     error_json_encode="-101"
     error_db_insert="-102"
     error_check_request_system="-120"
+    error_check_bill_type="-121"
 )
 func check_request_system(request_system int32)error {
     if request_system!=1{
         return errors.New("request_system !=1")
+    }
+    return nil
+}
+func check_bill_type() {
+    if bill_type!="Purchase Order"{
+        return errors.New("bill_type!="Purchase Order"")
     }
     return nil
 }
@@ -20,6 +27,10 @@ func check_data(origi *DeliverGoodsForPO)(string,error) {
     err=check_request_system(origi.Data.Request_system)
     if err!=nil{
         return error_check_request_system,err
+    }
+    err=check_bill_type(origi.Data.Purchase_order.Bill_type)
+    if err!=nil{
+        return error_check_bill_type,err
     }
     return "",nil
 }
