d9afe6458d9783dc364d03ec3b0da7ee2704fede
lzxm160 <lzxm160@sina.com>
2017-02-22 13:54:22 +0800

    init

--- src/mainproject/check_data.go
+++ src/mainproject/check_data.go
@@ -7,10 +7,19 @@ const(
     error_json_decode="-100"
     error_json_encode="-101"
     error_db_insert="-102"
-    error_check="-103"
+    error_check_request_system="-120"
 )
+func check_request_system(request_system int32)error {
+    if request_system!=1{
+        return errors.New("request_system !=1")
+    }
+}
 func check_data(origi *DeliverGoodsForPO)(string,error) {
-
-    return error_check,errors.New("can't work with -103")
+    var err error
+    err=check_request_system(origi.Data.Request_system)
+    if err!=nil{
+        return error_check_request_system,err
+    }
+    
     
 }
