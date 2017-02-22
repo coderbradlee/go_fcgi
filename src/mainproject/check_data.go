e10207e876eef2e3c903bb8eba5d1740c20cc3d8
lzxm160 <lzxm160@sina.com>
2017-02-22 14:24:23 +0800

    init

--- src/mainproject/check_data.go
+++ src/mainproject/check_data.go
@@ -2,6 +2,7 @@
  import (
     // "time"
     "errors"
+    "os"
 )
 const(
     error_json_decode="-100"
@@ -10,6 +11,7 @@ const(
     error_check_request_system="-120"
     error_check_bill_type="-121"
     error_check_po_no="-122"
+    error_check_po_url="-123"
 )
 func check_request_system(request_system int32)error {
     if request_system!=1{
@@ -30,6 +32,10 @@ func check_po_no(po_no string)error {
     }
     return nil
 }
+func check_po_url(po_url string)error {
+    _.err:=os.Stat(po_url)
+    return err
+}
 func check_data(origi *DeliverGoodsForPO)(string,error) {
     var err error
     err=check_request_system(origi.Data.Request_system)
@@ -43,6 +49,10 @@ func check_data(origi *DeliverGoodsForPO)(string,error) {
     err=check_po_no(origi.Data.Purchase_order.Po_no)
     if err!=nil{
         return error_check_po_no,err
+    }
+    err=check_po_url(origi.Data.Purchase_order.Po_url)
+    if err!=nil{
+        return error_check_po_url,err
     }
     return "",nil
 }
