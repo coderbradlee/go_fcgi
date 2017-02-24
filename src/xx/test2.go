d33a08d3c61d084060f1b1aaa2aec596a158138c
lzxm160 <lzxm160@sina.com>
2017-02-24 14:03:11 +0800

    init

--- src/xx/test2.go
+++ src/xx/test2.go
@@ -19,7 +19,7 @@ func handler(w http.ResponseWriter,r *http.Request) {
 	mu.Unlock()
 	fmt.Fprintf(w,"path=%q\n",r.URL.Path)
 }
-func count(w http.ResponseWriter,r *http.Request) {
+func counter(w http.ResponseWriter,r *http.Request) {
 	mu.Lock()
 	fmt.Fprintf(w,"count:%d\n",count)
 	mu.Unlock()
