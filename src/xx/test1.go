69e55632f97e10fe9cec80daea10a087918b9362
lzxm160 <lzxm160@sina.com>
2017-02-24 13:43:22 +0800

    init

--- src/xx/test1.go
+++ src/xx/test1.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	// "bytes"
-	// "fmt"
-	// stdLog "log"
-	// "path/filepath"
-	// "runtime"
-	// "strconv"
-	// "strings"
-	// "testing"
-	// "time"
-	// "logger"
-	// "strconv"
-	// // "testing"
-	// "time"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
 )
 
 func main() {
@@ -21,7 +14,29 @@ func main() {
 	test_log()
 }
 func test_log() {
-	
-	
-	
+	start:=time.Now()
+	ch:=make(chan string)
+	for _,url:=range os.Args[1:]{
+		go fetch(url,ch)
+	}
+	for range os.Args[1:]{
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("%.2fs elapsed\n",time.Since(start).Seconds())
+}
+func fetch(url string,ch chan<-string) {
+	start:=time.Now()
+	resp,err:=http.Get(url)
+	if err!=nil{
+		ch<-fmt.Sprint(err)
+		return
+	}
+	nbytes,err:=io.Copy(ioutil.Discard,resp.Body)
+	resp.Body.Close()
+	if err!=nil{
+		ch<-fmt.Sprintf("reading err:%s %v",url,err)
+		return
+	}
+	secs:=time.Since(start).Seconds()
+	ch<-fmt.Sprintf("%.2f %7d %s",secs,nbytes,url)
 }
