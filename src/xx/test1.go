ec990df1e905ab2de1323de96509d492cdb9895f
lzxm160 <lzxm160@sina.com>
2017-02-24 13:24:46 +0800

    init

---
+++ src/xx/test1.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	// "bytes"
+	// "fmt"
+	// stdLog "log"
+	// "path/filepath"
+	// "runtime"
+	// "strconv"
+	// "strings"
+	// "testing"
+	// "time"
+	// "logger"
+	// "strconv"
+	// // "testing"
+	// "time"
+)
+
+func main() {
+	// lissajous(os.Stdout)
+	test_log()
+}
+func test_log() {
+	
+	
+	
+}
