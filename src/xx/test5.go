6cbd831f0b2be4880ed9f1bccb32751edca03f73
lzxm160 <lzxm160@sina.com>
2017-02-27 15:09:15 +0800

    init

---
+++ src/xx/test5.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"bytes"
+	"bufio"
+	"os"
+	"io"
+)
+type Job struct{
+	filename string
+	results chan<- Results
+}
+func (j *Job)Do(reg *regexp.Regexp) {
+	// fmt.Println("Do")
+	// var lino int32=1111
+	// j.results<-Results{j.filename,lino,"11"}
+	file,err:=os.Open(j.filename)
+	if err!=nil{
+		fmt.Println("error:%s",err)
+	}
+	defer file.Close()
+	reader:=bufio.NewReader(file)
+	var lino int32
+	for lino=1;;lino++{
+		line,err:=reader.ReadBytes('\n')
+		// func (b *Reader) ReadBytes(delim byte) ([]byte, error)
+		line=bytes.TrimRight(line,"\r\n")
+		if reg.Match(line){
+			j.results<-Results{j.filename,lino,string(line)}
+		}
+		if err!=nil{
+			if err!=io.EOF{
+				fmt.Println("err:%d:%s",lino,err)
+			}
+			break;
+		}
+	}
+}
+type Results struct{
+	filename string
+	lino int32
+	line string
+}
+func addJobs(jobs chan<- Job,filename string,results chan<- Results) {
+	jobs<- Job{filename,results}
+	close(jobs)
+}
+func doJobs(done chan<- struct{},reg *regexp.Regexp,jobs <-chan Job) {
+	for job:=range jobs{
+		job.Do(reg)
+	}
+	done<-struct{}{}
+}
+func waitCompletion(timeout int64,done chan<- struct{},results <-chan Results) {
+	finish:=time.After(time.Duration(timeout))
+
+	for i:=0;i<8;i++{
+		select{
+			case r:=<-results:
+				fmt.Printf("%s:%d:%s\n",r.filename,r.lino,r.line)
+			case <-done:
+				fmt.Printf("done:%d:::::::::::\n",i)
+			case <-finish:
+				fmt.Printf("timeout\n")
+				return
+		}
+	}
+
+	for{
+		select{
+		case r:=<-results:
+			fmt.Printf("%s:%d:%s\n",r.filename,r.lino,r.line)
+		case <-finish:
+			fmt.Printf("timeout\n")
+			return
+		default:
+			return
+		}
+	}
+}
+func grep(reg *regexp.Regexp,filename string) {
+	jobs:=make(chan Job,8)
+	results:=make(chan Results,1000)
+	done:=make(chan struct{},8)
+	go addJobs(jobs,filename,results)
+	for i:=0;i<8;i++{
+		go doJobs(done,reg,jobs)
+	}
+	waitCompletion(10,done,results)
+}
+func main() {
+	reg:=regexp.MustCompile("flow.*")
+	grep(reg,"/root/redisRenesola-cluster-debug/cache_20170224_00208.log")
+}
