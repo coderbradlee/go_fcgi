ca0fe155ac9f8fb51d60c0a5f309cd066bc9f2b4
lzxm160 <lzxm160@sina.com>
2017-02-28 13:47:59 +0800

    init

--- src/README.md
+++ src/README.md
@@ -7,3 +7,5 @@ curl -X POST http://127.0.0.1:9888/po/deliver_goods -d '{"xx":"xxxx"}'
 curl -X GET http://172.18.100.85/pdf
 ab -n 1000000 -k -r -c 100 http://127.0.0.1/pdf
 ./weighttp -n 500000 -c 100 -t 2 -k 127.0.0.1/pdf
+
+./weighttp -n 500000 -c 100 -t 2 -k 127.0.0.1:9888/redis
---
+++ src/xx/test8.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"fmt"
+)
+type SafeMap interface{
+	Insert(string,interface{})
+	Delete(string)
+	Find(string)(interface{},bool)
+	Len()int32
+	Update(string,UpdateFunc)
+	Close()map[string]interface{}
+}
+type UpdateFunc func(interface{},bool)interface{}
+type safeMap chan commandData
+type commandData struct{
+	action commandAction
+	key string
+	value interface{}
+	result chan<- interface{}
+	data chan<- map[string]interface{}
+	updater UpdateFunc
+}
+type commandAction int
+const (
+	remove commandAction=iota
+	end 
+	find
+	insert
+	length
+	update
+)
+func (sm safeMap)Insert(key string,value interface{}) {
+	(sm)<-commandData{action:insert,key:key,value:value}
+}
+func (sm safeMap)Delete(key string) {
+	(sm)<-commandData{action:remove,key:key}
+}
+type findResult struct{
+	value interface{}
+	found bool
+}
+func (sm safeMap)Find(key string)(value interface{},found bool) {
+	reply:=make(chan interface{})
+	(sm)<-commandData{action:find,key:key,result:reply}
+	result:=(<-reply).(findResult)
+	return result.value,result.found
+}
+func (sm safeMap)Len()int {
+	reply:=make(chan interface{})
+	(sm)<-commandData{action:length,result:reply}
+	return (<-reply).(int)
+}
+func (sm safeMap)Update(key string,updater UpdateFunc){
+	(sm)<-commandData{action:update,key:key,updater:updater}
+}
+func (sm safeMap)Close()map[string]interface{} {
+	reply:=make(chan map[string]interface{})
+	(sm)<-commandData{action:end,data:reply}
+	return <-reply
+}
+func New()(safeMap) {
+	sm:=make(safeMap)
+	go sm.run()
+	return sm
+}
+func (sm safeMap)run() {
+	store:=make(map[string]interface{})
+	for command:=range sm{
+		switch command.action{
+		case insert:
+			store[command.key]=command.value
+		case remove:
+			delete(store,command.key)
+		case find:
+			value,found:=store[command.key]
+			command.result<-findResult{value,found}
+		case length:
+			command.result<-len(store)
+		case update:
+			value,found:=store[command.key]
+			store[command.key]=command.updater(value,found)
+		case end:
+			close(sm)
+			command.data<-store
+		}
+	}
+}
+func main() {
+	// test :=New()
+	// test.Insert("1",2)
+	// if data,found:=test.Find("1");found{
+	// 	fmt.Println(data)
+	// }
+	filename:="/root/redisRenesola-cluster-debug/logs/cache_20170224_00208.log"
+	var workers=runtime.NumCPU()
+	runtime.GOMAXPROCS(workers)
+	lines:=make(chan string,workers*4)
+	done:=make(chan struct{},workers)
+	pageMap:=New()
+	go readlines(filename,lines)
+	processLines(done,pageMap,lines)
+	waitUntil(done)
+	show(pageMap)
+}
+func readlines(filename string,lines chan<- string) {
+	file,err:=os.Open(filename)
+	if err!=nil{
+		fmt.Println(err)
+	}
+	defer file.Close()
+	reader:=bufio.NewReader(file)
+	for{
+		line,err:=reader.ReadString('\n')
+		if line!=""{
+			lines<-line
+		}
+		if err!=nil{
+			if err!=io.EOF{
+				fmt.Println(err)
+			}
+			break;
+		}
+	}
+}
+func processLines(done chan<- struct{},pageMap safeMap,lines <-chan string){
+	reg:=regexp.MustCompile('flow.?')
+	incrementer:=func(value interface{},found bool)interface {
+		if found{
+			return value.(int)+1
+		}
+		return 1
+	}
+	for i:=0;i<8;i++{
+		go func(){
+			for line:=lines{
+				if matches:=reg.FindStringSubmatch(line);matches!=nil{
+					pageMap.Update(matches[1],incrementer)
+				}
+			}
+			done<-struct{}{}
+		}()
+
+	}
+}
+func waitUntil(done <-chan struct{}) {
+	for i:=0;i<8;i++{
+		<-done
+	}
+}
+func show(pm safeMap) {
+	pages:=pm.Close()
+	for page,count:=range pm{
+		fmt.Printf("%8d %s\n",count,page)
+	}
+}
