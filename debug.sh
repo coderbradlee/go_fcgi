0437fb7667f0abdf0125f863c4c8c676e27aaa79
lzxm160 <lzxm160@sina.com>
2017-02-28 14:32:48 +0800

    init

--- debug.sh
+++ debug.sh
@@ -1,7 +1,3 @@
 export GOPATH=/root/go_fcgi
-export GOMAXPROCS=1
-export MARTINI_ENV=production
-rm -fr go_fcgi
-go build -o go_fcgi mainproject
-go build -o xx xx
-./go_fcgi
+go build -gcflags "-N -l" src/xx/test8.go
+gdb test8
--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 export GOPATH=/root/go_fcgi
-go build -gcflags "-N -l" src/xx/test8.go
-gdb test8
+export GOMAXPROCS=1
+export MARTINI_ENV=production
+rm -fr go_fcgi
+go build -o go_fcgi mainproject
+go build -o xx xx
+./go_fcgi
