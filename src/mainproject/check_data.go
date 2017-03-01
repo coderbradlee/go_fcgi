3947d14ea9d8de42832059e3fe0d9d1837e707b4
lzxm160 <lzxm160@sina.com>
2017-03-01 14:16:30 +0800

    init

--- src/mainproject/check_data.go
+++ src/mainproject/check_data.go
@@ -52,13 +52,14 @@ func check_supplier(supplier string)error {
     return nil
 }
 func check_packing_method(deliver_notes []Deliver_notes)error {
-    for d:=range deliver_notes{
-        var packing_method string
-        db.QueryRow("select packing_method_id from t_packing_method where native_name=?",d.Packing_method).Scan(&packing_method)
-        if packing_method== ""{
-            return errors.New(`packing_method_id missed`)
-        }
-    }
+    fmt.Println(deliver_notes[0].Packing_method)
+    // for d:=range deliver_notes{
+    //     var packing_method string
+    //     db.QueryRow("select packing_method_id from t_packing_method where native_name=?",d.Packing_method).Scan(&packing_method)
+    //     if packing_method== ""{
+    //         return errors.New(`packing_method_id missed`)
+    //     }
+    // }
     return nil
 }
 func check_data(origi *DeliverGoodsForPO)(string,error) {
