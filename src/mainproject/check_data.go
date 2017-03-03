469e202e8d6916678933d32531a033de878ba250
lzxm160 <lzxm160@sina.com>
2017-03-03 10:05:07 +0800

    init

--- src/mainproject/check_data.go
+++ src/mainproject/check_data.go
@@ -19,99 +19,90 @@ const(
     error_check_packing_method="-126"//t_packing_method表里没有此packing_method
     error_check_logistic_provider="-127"//物流提供商信息缺失
 )
-func check_request_system(request_system int32)error {
+func check_request_system(request_system int32,error_chan chan<- map[string]error) {
     if request_system!=1{
-        return errors.New("request_system !=1")
+        error_chan<- map[error_check_request_system]errors.New("request_system !=1")
     }
-    return nil
 }
-func check_bill_type(bill_type string)error {
-    if bill_type!="Purchase Order"{
-        return errors.New(`bill_type!=Purchase Order`)
-    }
-    return nil
-}
-func check_po_no(po_no string)error {
-    cs:="PO-FR-20170216-00101"
-    if len(po_no)>len(cs){
-        return errors.New(`po_no is too long`)
-    }
-    return nil
-}
-func check_po_url(po_url string)error {
-    _,err:=os.Stat(po_url)
-    return err
-}
-func check_status(status int32)error {
-    if status!=1{
-        return errors.New(`status!=1`)
-    }
-    return nil
-}
-func check_supplier(supplier string)error {
-    if supplier!="Renesola Shanghai"{
-        return errors.New(`supplier is not Renesola Shanghai`)
-    }
-    return nil
-}
-func check_packing_method(deliver_notes []Deliver_notes)error {
-    // fmt.Println(deliver_notes[0].Packing_method)
-    for _,d:=range deliver_notes{
-        // fmt.Println(reflect.TypeOf(d))
-        var packing_method string
-        db.QueryRow("select packing_method_id from t_packing_method where name=?",d.Packing_method).Scan(&packing_method)
-        if packing_method== ""{
-            return errors.New(`packing_method_id missed`)
-        }
-    }
-    return nil
-}
-func check_logistic_provider(deliver_notes []Deliver_notes)error {
-    // fmt.Println(deliver_notes[0].Packing_method)
-    for _,d:=range deliver_notes{
-        // fmt.Println(reflect.TypeOf(d))
-        var logistic_provider_basic_id string
-        db.QueryRow("select logistic_provider_basic_id from t_logistic_provider_basic where name=?",d.Logistic).Scan(&logistic_provider_basic_id)
-        if logistic_provider_basic_id== ""{
-            return errors.New(`logistic_provider_basic_id missed`)
-        }
-    }
-    return nil
-}
+// func check_bill_type(bill_type string,error_chan chan<- map[string]error) {
+//     if bill_type!="Purchase Order"{
+//         error_chan<- errors.New(`bill_type!=Purchase Order`)
+//     }
+// }
+// func check_po_no(po_no string,error_chan chan<- map[string]error) {
+//     cs:="PO-FR-20170216-00101"
+//     if len(po_no)>len(cs){
+//         error_chan<- errors.New(`po_no is too long`)
+//     }
+// }
+// func check_po_url(po_url string,error_chan chan<- map[string]error) {
+//     _,err:=os.Stat(po_url)
+//     error_chan<-err
+// }
+// func check_status(status int32,error_chan chan<- map[string]error) {
+//     if status!=1{
+//         error_chan<- errors.New(`status!=1`)
+//     }
+// }
+// func check_supplier(supplier string,error_chan chan<- map[string]error) {
+//     if supplier!="Renesola Shanghai"{
+//         error_chan<- errors.New(`supplier is not Renesola Shanghai`)
+//     }
+// }
+// func check_packing_method(deliver_notes []Deliver_notes,error_chan chan<- map[string]error) {
+//     // fmt.Println(deliver_notes[0].Packing_method)
+//     for _,d:=range deliver_notes{
+//         // fmt.Println(reflect.TypeOf(d))
+//         var packing_method string
+//         db.QueryRow("select packing_method_id from t_packing_method where name=?",d.Packing_method).Scan(&packing_method)
+//         if packing_method== ""{
+//             error_chan<- errors.New(`packing_method_id missed`)
+//         }
+//     }
+// }
+// func check_logistic_provider(deliver_notes []Deliver_notes,error_chan chan<- map[string]error) {
+//     // fmt.Println(deliver_notes[0].Packing_method)
+//     for _,d:=range deliver_notes{
+//         // fmt.Println(reflect.TypeOf(d))
+//         var logistic_provider_basic_id string
+//         db.QueryRow("select logistic_provider_basic_id from t_logistic_provider_basic where name=?",d.Logistic).Scan(&logistic_provider_basic_id)
+//         if logistic_provider_basic_id== ""{
+//             error_chan<- errors.New(`logistic_provider_basic_id missed`)
+//         }
+//     }
+// }
 func check_data(origi *DeliverGoodsForPO)(string,error) {
     var err error
-    err=check_request_system(origi.Data.Request_system)
-    if err!=nil{
-        return error_check_request_system,err
-    }
-    err=check_bill_type(origi.Data.Purchase_order.Bill_type)
-    if err!=nil{
-        return error_check_bill_type,err
-    }
-    // err=check_po_no(origi.Data.Purchase_order.Po_no)
+    var error_chan make(chan map[string]error)
+    check_request_system(origi.Data.Request_system,error_chan)
+    // check_bill_type(origi.Data.Purchase_order.Bill_type)
     // if err!=nil{
-    //     return error_check_po_no,err
+    //     return error_check_bill_type,err
     // }
-    err=check_po_url(origi.Data.Purchase_order.Po_url)
-    if err!=nil{
-        return error_check_po_url,err
-    }
-    err=check_status(origi.Data.Purchase_order.Status)
-    if err!=nil{
-        return error_check_status,err
-    }
-    err=check_supplier(origi.Data.Purchase_order.Supplier)
-    if err!=nil{
-        return error_check_supplier,err
-    }
-    err=check_packing_method(origi.Data.Deliver_notes)
-    if err!=nil{
-        return error_check_packing_method,err
-    }
-    err=check_logistic_provider(origi.Data.Deliver_notes)
-    if err!=nil{
-        return error_check_logistic_provider,err
-    }
+    // // err=check_po_no(origi.Data.Purchase_order.Po_no)
+    // // if err!=nil{
+    // //     return error_check_po_no,err
+    // // }
+    // err=check_po_url(origi.Data.Purchase_order.Po_url)
+    // if err!=nil{
+    //     return error_check_po_url,err
+    // }
+    // err=check_status(origi.Data.Purchase_order.Status)
+    // if err!=nil{
+    //     return error_check_status,err
+    // }
+    // err=check_supplier(origi.Data.Purchase_order.Supplier)
+    // if err!=nil{
+    //     return error_check_supplier,err
+    // }
+    // err=check_packing_method(origi.Data.Deliver_notes)
+    // if err!=nil{
+    //     return error_check_packing_method,err
+    // }
+    // err=check_logistic_provider(origi.Data.Deliver_notes)
+    // if err!=nil{
+    //     return error_check_logistic_provider,err
+    // }
     
     return "",nil
 }
