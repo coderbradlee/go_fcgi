ce3110d92b4f8d4bf014479cbd4d4e5022d2f9ad
lzxm160 <lzxm160@sina.com>
2017-03-03 15:31:47 +0800

    init

--- src/mainproject/goods_delivery_note_attachment.go
+++ src/mainproject/goods_delivery_note_attachment.go
@@ -23,22 +23,31 @@ func insert_goods_delivery_note_attachment(file_name,url,language string,sort_no
         1)
     return err
 }
-func get_language_id(company string)string{
+func get_language_id_chan(language_id_chan chan<- string,company string)string{
     var language_id string
     db.QueryRow("select default_language_id from t_company where short_name=?",company).Scan(&language_id)
-    return language_id
+    language_id_chan<- language_id
 }
-func get_sort_no()int {
+func get_sort_no_chan(sort_no_chan chan<- int) {
     var sortno int 
     db.QueryRow("select sort_no from t_goods_delivery_note_attachment ORDER BY sort_no desc LIMIT 1").Scan(&sortno)
-    return sortno
+    sort_no_chan<-sortno
 }
 func insert_note_attachment(
     t *purchase_order,
     origi *DeliverGoodsForPO,sd *shared_data)error {
     var err error
-    language:=get_language_id(origi.Data.Purchase_order.Company)
-    sort_no:=get_sort_no()
+    // language:=get_language_id(origi.Data.Purchase_order.Company)
+    language_chan :=make(chan string)
+    go get_language_id_chan(language_chan,origi.Data.Purchase_order.Company)
+    // language:=<-language_chan
+/////////////////////////////////////////////
+    // sort_no:=get_sort_no()
+    sort_no_chan :=make(chan int)
+    go get_sort_no_chan(sort_no_chan)
+    sort_no:=<-sort_no_chan
+    language:=<-language_chan
+    
     for _,d:= range origi.Data.Deliver_notes{
         err= insert_goods_delivery_note_attachment(d.Commercial_invoice.Ci_no,d.Commercial_invoice.Ci_url,language,sort_no+1,sd)
         
