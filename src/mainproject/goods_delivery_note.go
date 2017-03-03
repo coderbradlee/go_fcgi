3a47b76e1224513ed5d860322010e193db7ada21
lzxm160 <lzxm160@sina.com>
2017-03-03 20:43:11 +0800

    line

--- src/mainproject/goods_delivery_note.go
+++ src/mainproject/goods_delivery_note.go
@@ -225,5 +225,5 @@ func insert_goods_delivery_note(level3_chan chan<- error,t *purchase_order,origi
         logger.Info("insert_goods_delivery_note:"+err.Error()) 
     }
     level3_chan<-err
-
+    fmt.Println("goods_delivery_note.go 228")
 }
