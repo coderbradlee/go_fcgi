d1d6504f6b692223b60e15986c462ebbb2f7691a
lzxm160 <lzxm160@sina.com>
2017-03-06 09:32:24 +0800

    decode

--- src/mainproject/main.go
+++ src/mainproject/main.go
@@ -151,9 +151,9 @@ func startMartini() {
     // return auth_SecureCompare(username, "admin") && auth_SecureCompare(password, "admin")
     
     // }))
-    m.Use(auth_BasicFunc(func(username, password string) bool {
-        return username == "admin" && password == "admin"
-    }))
+    // m.Use(auth_BasicFunc(func(username, password string) bool {
+    //     return username == "admin" && password == "admin"
+    // }))
     m.Post("/po/deliver_goods",poHandler)
     m.RunOnAddr(port)
     ////////////////////
