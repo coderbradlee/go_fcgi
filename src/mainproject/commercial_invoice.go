59b126ccccebed3dcc2a714e4f2ae2c8b15042e6
lzxm160 <lzxm160@sina.com>
2017-03-06 15:06:49 +0800

    return string error

--- src/mainproject/commercial_invoice.go
+++ src/mainproject/commercial_invoice.go
@@ -27,7 +27,7 @@ func insert_ci(ci *Commercial_invoice,t *purchase_order,
     // purchase_order_id:=<-purchase_order_id_chan
 
 /////////////////////////////////////////////////////////////////
-    if ci.status!=1{
+    if ci.Status!=1{
         return error_commercial_invoice_status,err.New("commercial_invoice.status!=1")
     }
     _, err = db.Exec(
