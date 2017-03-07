9e65fde3f4321f4b4af1f55db027961d97cad1d2
lzxm160 <lzxm160@sina.com>
2017-03-07 12:36:23 +0800

    cgo

--- src/mainproject/pdf.go
+++ src/mainproject/pdf.go
@@ -1,10 +1,15 @@
  package main
- import (
+/*
+#include "wkhtmltox/pdf.h"
+*/
+import "C"
+import (
     _"log"
     "fmt"
-    
     "net/http"
 )
+func Convert()
 func pdfHandler (w http.ResponseWriter, r *http.Request) {
   fmt.Fprint(w, "pdf!")
+  Convert()
 } 
