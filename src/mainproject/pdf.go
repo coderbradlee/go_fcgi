808c80a6396dd29c16983b2ca7863176ecdb2396
lzxm160 <lzxm160@sina.com>
2017-03-09 15:20:39 +0800

    err

--- src/mainproject/pdf.go
+++ src/mainproject/pdf.go
@@ -118,7 +118,7 @@ func convert(src,dst string) error {
 	os.Set("load.blockLocalFileAccess","false") 
 	os.Set("load.loadErrorHandling","skip")
 
-	os.Set("toc.forwardLinks","true")
+	// os.Set("toc.forwardLinks","true")
 	os.Set("useLocalLinks", "true")
 	os.Set("produceForms", "true")
 
