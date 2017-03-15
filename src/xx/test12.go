4895375e105b4d53a101cf615b4d1cbb38c1efc0
lzxm160 <lzxm160@sina.com>
2017-03-15 17:27:01 +0800

    receiver

--- src/xx/test12.go
+++ src/xx/test12.go
@@ -7,63 +7,28 @@ import (
 	// "bufio"
 	// "runtime"
 	// "io"
-	"sync"
+	// "sync"
 	// "testing"
 )
-var lock sync.Mutex
-func test() {
-	lock.Lock()
-	lock.Unlock()
+type User struct{
+	id int
+	name string
 }
-func test_defer() {
-	lock.Lock()
-	defer lock.Unlock()
+type Manager struct{
+	User
 }
-func test_panic() {
-	defer func () {
-		if err:=recover();err!=nil{
-			fmt.Println(err.(string))
-		}
-	}()
-	panic("panic error")
+func (self *User)ToString()string {
+	return fmt.Sprintf("user:%p,%v",self,self)
 }
-func test_slice() {
-	data:=[...]int{0,1,2,3,10:0}
-	s:=data[:2:3]
-	fmt.Println(&s[0],&data[0])
-	fmt.Println(s,data)
-	s=append(s,200)
-	fmt.Println(&s[0],&data[0])
-	fmt.Println(s,data)
-	s=append(s,300,400)
-	fmt.Println(&s[0],&data[0])
-	fmt.Println(s,data)
+func (self *Manager)ToString()string {
+	return fmt.Sprintf("user:%p,%v",self,self)
 }
-func test_map() {
-	m:=map[string]int{
-		"a":1,
-	}
-	if v,ok:=m["a"];ok{
-		fmt.Println(v)
-	}
-	fmt.Println(m["c"])
-	m["b"]=2
-	delete(m,"c")
-	fmt.Println(len(m))
-	for k,v:=range m{
-		fmt.Println(k,v)
-	}
+func test_override() {
+	m:=Manager{User{1,"tom"}}
+	fmt.Println(m.ToString())
+	fmt.Println(m.User.ToString())
 }
-type X struct{}
-
-func (*X)test_receiver() {
-	fmt.Println("receiver")
-}
-
 func main() {
-	// test_defer(0)
-	// test_map()
-	p:=&X{}
-	p.test_receiver()
+
 }
 
