7d7307b057715c42f1c0918f9d287283c6af65e8
lzxm160 <lzxm160@sina.com>
2017-03-15 11:48:20 +0800

    pdf

--- src/xx/test12.go
+++ src/xx/test12.go
@@ -9,35 +9,15 @@ import (
 	// "io"
 	// "sync"
 )
-func test(s string,n ...int)string {
-	var x int
-	for _,i:=range n{
-		x+=i
-	}
-	return fmt.Sprintf(s,x)
-}
-func add(x,y int)(z int) {
-	defer func() {
-		fmt.Println("defer")
-		z+=100
+func test_defer() {
+	defer fmt.Println("a")
+	defer fmt.Println("b")
+	defer func () {
+		fmt.Println(100/0)
 	}()
-	z=x+y
-	fmt.Println("return")
-	return
+	defer fmt.Println("c")
 }
 func main() {
-	
-	a:=[3]int{0,1,2}
-	for i,v:=range a{
-		if i==0{
-			a[1],a[2]=998,999
-			fmt.Println(a)
-		}
-		a[i]=v+100
-	}
-	fmt.Println(a)
-	aa:=[]int{0,1,2}
-	fmt.Println(test("sum:%d",aa...))
-	fmt.Println(add(1,2))
+	test_defer()
 }
 
