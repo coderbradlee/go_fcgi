9574f70fac723704e7cc6b42f40ac01fb4355e63
lzxm160 <lzxm160@sina.com>
2017-03-15 13:06:46 +0800

    pdf

--- src/xx/test12.go
+++ src/xx/test12.go
@@ -9,15 +9,15 @@ import (
 	// "io"
 	// "sync"
 )
-func test_defer() {
+func test_defer(a int) {
 	defer fmt.Println("a")
 	defer fmt.Println("b")
 	defer func () {
-		fmt.Println(100/0)
+		fmt.Println(100/a)
 	}()
 	defer fmt.Println("c")
 }
 func main() {
-	test_defer()
+	test_defer(0)
 }
 
