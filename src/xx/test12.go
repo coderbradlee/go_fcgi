2692bfd36a8a08132bdc96c9fc90901cdace614f
lzxm160 <lzxm160@sina.com>
2017-03-15 14:06:27 +0800

    pdf

--- src/xx/test12.go
+++ src/xx/test12.go
@@ -19,16 +19,7 @@ func test_defer() {
 	lock.Lock()
 	defer lock.Unlock()
 }
-func benchmarkTest(b *testing.B) {
-	for i:=0;i<b.N;i++{
-		test()
-	}
-}
-func benchmarkTestDefer(b *testing.B) {
-	for i:=0;i<b.N;i++{
-		test_defer()
-	}
-}
+
 func main() {
 	// test_defer(0)
 }
--- src/xx/test12_test.go
+++ src/xx/test12_test.go
@@ -1,24 +1,9 @@
 package main
 
 import (
-	// "fmt"
-	// "regexp"
-	// "os"
-	// "bufio"
-	// "runtime"
-	// "io"
-	"sync"
 	"testing"
 )
-var lock sync.Mutex
-func test() {
-	lock.Lock()
-	lock.Unlock()
-}
-func test_defer() {
-	lock.Lock()
-	defer lock.Unlock()
-}
+
 func Benchmark_Test(b *testing.B) {
 	for i:=0;i<b.N;i++{
 		test()
