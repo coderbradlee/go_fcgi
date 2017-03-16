1e13543fabdf3cd10bbec00e0d0db554ed49b653
lzxm160 <lzxm160@sina.com>
2017-03-16 10:23:08 +0800

    interface

--- src/xx/test12.go
+++ src/xx/test12.go
@@ -18,14 +18,6 @@ func (self User)String()string {
 	return fmt.Sprintf("%d,%s",self.id,self.name)
 }
 func main() {
-	var empty interface{}=User{1,"tom"}
-	switch v:=empty.(type){
-		case nil:
-			fmt.Println("nil")
-		case fmt.Stringer:
-			fmt.Println(v)
-		case func()string:
-			fmt.Println(v())
-	}
+	var _ fmt.Stringer=(*User)(nil)
 }
 
