84bf1a60008fbfd3119c79bd13fa4c62be777526
lzxm160 <lzxm160@sina.com>
2017-03-16 15:12:20 +0800

    interface

--- src/xx/test12.go
+++ src/xx/test12.go
@@ -25,13 +25,16 @@ func init() {
 func main() {
 	
 	wg:=new(sync.WaitGroup)
-	wg.Add(2)
-	for i:=0;i<2;i++{
-		go func (id int) {
-			defer wg.Done()
-			sum(id)
-		}(i)
-	}
+	wg.Add(1)
+	go func () {
+		defer wg.Done()
+		defer fmt.Println("defereee")
+		func () {
+			defer fmt.Println("eeeee")
+			runtime.Goexit()
+		}()
+		fmt.Println("fffffff")
+	}()
 	wg.Wait()
 }
 
