c2e20af032d1626bb33dff724396e1f854901ecd
lzxm160 <lzxm160@sina.com>
2017-03-18 12:29:20 +0800

    ok

--- src/xx/test12.go
+++ src/xx/test12.go
@@ -3,47 +3,29 @@ package main
 import (
 	"fmt"
 	// "regexp"
-	"os"
+	// "os"
 	// "bufio"
 	"runtime"
 	// "io"
 	// "sync"
 	// "testing"
-	"math"
+	// "math"
+	"time"
 )
-func sum(id int) {
-	var x int64
-	for i:=0;i<math.MaxUint32;i++{
-		x+=int64(i)
-	}
-	fmt.Println(id,x)
-}
+var now=time.Now()
+
 func init() {
 	// runtime.NumCPU()
 	runtime.GOMAXPROCS(8)
+	fmt.Println("init:",int(time.Now().Sub(now).Seconds()))
+	w:=make(chan bool)
+	go func () {
+		time.Sleep(time.Second*3)
+		w<-true
+	}()
+	<-w
 }
 func main() {
-	a,b:=make(chan int,3),make(chan int)
-	go func () {
-		v,ok,s:=0,false,""
-		for{
-			select{
-				case v,ok=<-a:s="a"
-				case v,ok=<-b:s="b"
-			}
-			if ok{
-				fmt.Println(s,v)
-			}else{
-				os.Exit(0)
-			}
-		}
-	}()
-	for i:=0;i<10;i++{
-		select{
-		case a<-i:
-		case b<-i:
-		}
-	}
-	close(a)
+	fmt.Println("main:",int(time.Now().Sub(now).Seconds()))
 }
 
