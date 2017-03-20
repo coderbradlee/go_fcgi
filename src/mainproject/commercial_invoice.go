2693c6c09001af285755a9f1e060b515eb3b5576
lzxm160 <lzxm160@sina.com>
2017-03-20 16:48:31 +0800

    split

--- src/mainproject/commercial_invoice.go
+++ src/mainproject/commercial_invoice.go
@@ -20,7 +20,7 @@ func insert_ci(ci *Commercial_invoice,
         payment_dead_line,payment_received,payment_due,shipping_cost_total,markup_total,tax_total,sub_total,grand_total,approvedBy,approvedAt,note,createAt,createBy,dr,data_version) 
         VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`,
         rand_string(20),
-        origi.Supplier,
+        "origi.Supplier",
         ci.Ci_no,
         ci.Ci_date,
         "",//sales_order_id
