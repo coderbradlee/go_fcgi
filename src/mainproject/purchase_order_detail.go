107f38c358b65ba43c908cbdae08048e1ac1dced
lzxm160 <lzxm160@sina.com>
2017-03-20 15:22:13 +0800

    split

--- src/mainproject/purchase_order_detail.go
+++ src/mainproject/purchase_order_detail.go
@@ -1,55 +1,55 @@
  package main
  import (
     // "fmt"
-    // "time"
-    // "errors"
+    "time"
+    "errors"
 )
 
 func insert_purchase_order_detail(t *purchase_order,origi *PoData,sd *shared_data)(string,error) {
-// 	var err error
-// 	for _,detail:= range origi.Data.Purchase_order.Detail{
-// 		// item_master_id:=get_item_master_id(detail.Item_no,detail.Product_name,detail.Product_code)
-// 		// uom_id:=get_uom_id(detail.Uom)
-// 		// fmt.Println(sd.company_time_zone)
-// 		// item_master_id:=get_item_master_id(detail.Item_no,detail.Product_name,detail.Product_code)
-//             item_master_id_chan :=make(chan string)
-//             go get_item_master_id_chan(item_master_id_chan,detail.Item_no,detail.Product_name,detail.Product_code)
-//             // item_master_id:=<-item_master_id_chan
-//             ////////////////////////////////////////
-//             // uom_id:=get_uom_id(detail.Uom)
+	var err error
+	for _,detail:= range origi.Data.Purchase_order.Detail{
+		// item_master_id:=get_item_master_id(detail.Item_no,detail.Product_name,detail.Product_code)
+		// uom_id:=get_uom_id(detail.Uom)
+		// fmt.Println(sd.company_time_zone)
+		// item_master_id:=get_item_master_id(detail.Item_no,detail.Product_name,detail.Product_code)
+            item_master_id_chan :=make(chan string)
+            go get_item_master_id_chan(item_master_id_chan,detail.Item_no,detail.Product_name,detail.Product_code)
+            // item_master_id:=<-item_master_id_chan
+            ////////////////////////////////////////
+            // uom_id:=get_uom_id(detail.Uom)
 
-//             uom_id_chan :=make(chan string)
-//             go get_uom_id_chan(uom_id_chan,detail.Uom)
-//             uom_id:=<-uom_id_chan
-//             item_master_id:=<-item_master_id_chan
-//             if uom_id==""{
-//             	return error_purchase_order_detail_uom_id,errors.New("purchase_order.detail uom_id is missed")
-//             }
-//             if item_master_id==""{
-//             	return error_purchase_order_detail_item_master_id,errors.New("purchase_order.detail item_master_id is missed")
-//             }
-// 		_, err = db.Exec(
-//         `INSERT INTO t_purchase_order_detail(detail_id,purchase_order_id,
-// 		item_master_id,unit_price,quantity,uom_id,sub_amount,warranty,
-// 		comments,note,createAt,createBy,dr,data_version) 
-// 		VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)`,
-// 		rand_string(20),
-// 		t.purchase_order_id,
-// 		item_master_id,
-// 		detail.Unit_price,
-// 		detail.Quantity,
-// 		uom_id,
-// 		detail.Sub_total,
-// 		detail.Warranty,
-// 		detail.Comments,
-// 		detail.Note,
-// 		time.Now().Add(sd.company_time_zone).Format("2006-01-02 15:04:05"),
-// 		"go_fcgi",
-// 		0,
-// 		1)
-// 	}
-// 	if err!=nil{
-// 		return error_insert_purchase_order_detail,err
-// 	}
+            uom_id_chan :=make(chan string)
+            go get_uom_id_chan(uom_id_chan,detail.Uom)
+            uom_id:=<-uom_id_chan
+            item_master_id:=<-item_master_id_chan
+            if uom_id==""{
+            	return error_purchase_order_detail_uom_id,errors.New("purchase_order.detail uom_id is missed")
+            }
+            if item_master_id==""{
+            	return error_purchase_order_detail_item_master_id,errors.New("purchase_order.detail item_master_id is missed")
+            }
+		_, err = db.Exec(
+        `INSERT INTO t_purchase_order_detail(detail_id,purchase_order_id,
+		item_master_id,unit_price,quantity,uom_id,sub_amount,warranty,
+		comments,note,createAt,createBy,dr,data_version) 
+		VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)`,
+		rand_string(20),
+		t.purchase_order_id,
+		item_master_id,
+		detail.Unit_price,
+		detail.Quantity,
+		uom_id,
+		detail.Sub_total,
+		detail.Warranty,
+		detail.Comments,
+		detail.Note,
+		time.Now().Add(sd.company_time_zone).Format("2006-01-02 15:04:05"),
+		t.Data.Purchase_order.Created_by+" go_fcgi",
+		0,
+		1)
+	}
+	if err!=nil{
+		return error_insert_purchase_order_detail,err
+	}
 	return "",nil
 }
