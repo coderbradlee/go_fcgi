314106814a2e1a0bfe840885ae0da0c5a3fd443c
lzxm160 <lzxm160@sina.com>
2017-03-20 19:31:21 +0800

    test

--- src/xx/test12_test.go
+++ src/xx/test12_test.go
@@ -2,16 +2,33 @@ package main
 
 import (
 	"testing"
+	"time"
 )
+func sum(n ...int)int {
+	var c int
+	for _,i:=range n{
+		c+=i
+	}
+	return c
+}
+func TestSum(t *testing.T) {
+	time.Sleep(time.Second*2)
+	if sum(1,2,3)!=6{
+		t.Fatal("sum error")
+	}
+}
+func TestTimeout(t *testing.T) {
+	time.Sleep(time.Second*5)
+}
 
-func Benchmark_Test(b *testing.B) {
-	for i:=0;i<b.N;i++{
-		test()
-	}
-}
-func Benchmark_TestDefer(b *testing.B) {
-	for i:=0;i<b.N;i++{
-		test_defer()
-	}
-}
+// func Benchmark_Test(b *testing.B) {
+// 	for i:=0;i<b.N;i++{
+// 		test()
+// 	}
+// }
+// func Benchmark_TestDefer(b *testing.B) {
+// 	for i:=0;i<b.N;i++{
+// 		test_defer()
+// 	}
+// }
 
