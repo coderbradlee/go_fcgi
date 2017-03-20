aadbc050aeed0a3f71d1e7adf6a576c00cfe7d5c
lzxm160 <lzxm160@sina.com>
2017-03-20 11:51:44 +0800

    destination_company_id_chan

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ export GOBIN=/root/go_fcgi
 export MARTINI_ENV=production
 
 rm -fr go_fcgi
-go build -race -o go_fcgi mainproject
+go build -o go_fcgi mainproject
 ./go_fcgi
 #go install mainproject
