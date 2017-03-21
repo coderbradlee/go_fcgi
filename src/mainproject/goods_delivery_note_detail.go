34620321dc1182856b63c6bb39e1c259bc5ae130
lzxm160 <lzxm160@sina.com>
2017-03-21 10:24:42 +0800

    gdn

--- src/mainproject/goods_delivery_note_detail.go
+++ src/mainproject/goods_delivery_note_detail.go
@@ -29,6 +29,7 @@ func insert_goods_delivery_note_detail(detail *Deliver_notes_detail,item_master_
         detail.Unit_price,
         "currency_id",
         detail.Sub_total,
+        "note"
         time.Now().Add(sd.company_time_zone).Format("2006-01-02 15:04:05"),
         "go_fcgi",
         0,
