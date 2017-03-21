6b222e828b82e90deaf62684cbc36e1c59d47c7a
lzxm160 <lzxm160@sina.com>
2017-03-21 15:31:11 +0800

    gdn

--- src/mainproject/goods_delivery_note.go
+++ src/mainproject/goods_delivery_note.go
@@ -191,7 +191,7 @@ func insert_goods_delivery_note(origi *DeliverGoodsForPO,sd *shared_data)(string
         logger.Info("insert_goods_delivery_note:"+err.Error()) 
         return error_insert_goods_delivery_note,err
     }else{
-        s,err= insert_note_detail(d,sd)
+        s,err:= insert_note_detail(deliver_notes,sd)
         if err!=nil{
             logger.Info(s+":"+err.Error()) 
             return s,err
