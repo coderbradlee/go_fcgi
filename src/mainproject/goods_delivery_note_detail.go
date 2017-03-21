92bbc3057242bbd14ead60c68a9bf30db789f12c
lzxm160 <lzxm160@sina.com>
2017-03-21 10:01:03 +0800

    gdn

---
+++ src/mainproject/goods_delivery_note_detail.go
@@ -0,0 +1,66 @@
+ package main
+ import (
+    "time"
+    "logger"
+)
+func insert_goods_delivery_note_detail(detail *Deliver_notes_detail,item_master_id,uom_id string,
+        delivery_qty int32,sd *shared_data)(string,error) {
+    var err error
+    _, err = db.Exec(
+        `INSERT INTO t_goods_delivery_note_detail(
+        detail_id,
+        goods_delivery_note_id,
+        item_master_id,
+        delivery_qty,
+        uom_id,
+        unit_price,
+        currency_id,
+        amount,
+        note,
+        createAt,
+        createBy,
+        dr,
+        data_version) 
+        VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)`,
+        rand_string(20),
+        sd.goods_delivery_note_id,
+        item_master_id,
+        detail.Quantity,
+        uom_id,
+        detail.Unit_price,
+        "currency_id",
+        detail.Sub_total,
+        time.Now().Add(sd.company_time_zone).Format("2006-01-02 15:04:05"),
+        "go_fcgi",
+        0,
+        1)
+    return error_insert_goods_delivery_note_detail,err
+}
+
+func insert_note_detail(
+    origi *DeliverGoodsForPO,sd *shared_data)(string,error) {
+    var err error
+    var s string
+    for _,d:= range origi.Data.Deliver_notes{
+        for _,detail:=range d.Detail{
+            // item_master_id:=get_item_master_id(detail.Item_no,detail.Product_name,detail.Product_code)
+            item_master_id_chan :=make(chan string)
+            go get_item_master_id_chan(item_master_id_chan,detail.Item_no,detail.Product_name,detail.Product_code)
+            // item_master_id:=<-item_master_id_chan
+            ////////////////////////////////////////
+            // uom_id:=get_uom_id(detail.Uom)
+
+            uom_id_chan :=make(chan string)
+            go get_uom_id_chan(uom_id_chan,detail.Uom)
+            uom_id:=<-uom_id_chan
+            item_master_id:=<-item_master_id_chan
+            
+            s,err= insert_goods_delivery_note_detail(&detail,item_master_id,uom_id,sd)
+            if err!=nil{
+                logger.Info("insert to insert_goods_delivery_note_detail:"+err.Error()) 
+                return s,err
+            }
+        }
+    }    
+    return error_insert_goods_delivery_note_detail,err
+}
---
+++ src/mainproject/goods_receipt.go
@@ -0,0 +1,56 @@
+ package main
+ import (
+    "time"
+    // "logger"
+)
+
+func insert_goods_receipt(
+    origi *DeliverGoodsForPO,sd *shared_data)(string,error) {
+    var err error
+    ////////////////////////////
+    bill_type_id_chan :=make(chan string)
+    go get_bill_type_id_chan(bill_type_id_chan,origi.Data.Purchase_order.Bill_type)
+    bill_type_id:=<-bill_type_id_chan
+    ///////////////////////////////
+    // company_id_chan :=make(chan string)
+    // go get_company_id_chan(company_id_chan,origi.Data.Purchase_order.Company)
+    // company_id:=<-company_id_chan
+
+    _, err = db.Exec(
+        `INSERT INTO t_goods_receipt(
+            receipt_id,
+            goods_receipt_no,
+            bill_type_id,
+            company_id,
+            purchase_order_id,
+            goods_delivery_note_id,
+            status,
+            receipt_date,
+            from_system_code,
+            approved_by,
+            approved_at,
+            note,
+            createAt,
+            createBy,
+            dr,
+            data_version) 
+        VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`,
+        rand_string(20),
+        sd.goods_receipt_no,
+        bill_type_id,
+        "company_id",
+        "purchase_order_id",
+        sd.goods_delivery_note_id,
+        0,//status
+        "",//receipt_date,
+        2,//from_system_code,
+        "approved_by",//approved_by,
+        "approved_at",//approved_at,
+        "note",//note
+        time.Now().Add(sd.company_time_zone).Format("2006-01-02 15:04:05"),
+        "go_fcgi",
+        0,
+        1)
+    return error_insert_goods_receipt,err
+}
+
