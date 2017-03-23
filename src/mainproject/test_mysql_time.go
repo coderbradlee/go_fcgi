e75173787852469bb1c1a154928a633606e225ed
lzxm160 <lzxm160@sina.com>
2017-03-23 17:55:42 +0800

    new json

--- src/mainproject/test_mysql_time.go
+++ src/mainproject/test_mysql_time.go
@@ -27,11 +27,11 @@ func test_mysql_time (w http.ResponseWriter, r *http.Request) {
 	    // log.Println(string(body))
 	    var t PoData  
 	    // var po_shared_data shared_data
-	    err_decode := json.Unmarshal(body, &t)
+	    json.Unmarshal(body, &t)
 	    // bytes.Trim(body,"\\r\\n")
 	    // line := strings.Trim(string(body), "\r\n")
 		defer r.Body.Close()
-		
+
 		ret:=single_select()
 	    fmt.Fprint(w,ret )
 	    log_str:=fmt.Sprintf("Started %s %s for %s:%s response:%s", r.Method, r.URL.Path, addr,"body",ret)
