ca2895eac152e50ea53409c3e2af395e191c2480
lzxm160 <lzxm160@sina.com>
2017-03-23 10:28:05 +0800

    new json

--- src/mainproject/purchase_order_detail.go
+++ src/mainproject/purchase_order_detail.go
@@ -15,22 +15,22 @@ func insert_purchase_order_detail(t *purchase_order,origi *PoData,sd *shared_dat
 		// uom_id:=get_uom_id(detail.Uom)
 		// fmt.Println(sd.company_time_zone)
 		// item_master_id:=get_item_master_id(detail.Item_no,detail.Product_name,detail.Product_code)
-            item_master_id_chan :=make(chan string)
-            go get_item_master_id_chan(item_master_id_chan,detail.Item_no,detail.Product_name,detail.Product_code)
-            // item_master_id:=<-item_master_id_chan
-            ////////////////////////////////////////
-            // uom_id:=get_uom_id(detail.Uom)
+        item_master_id_chan :=make(chan string)
+        go get_item_master_id_chan(item_master_id_chan,detail.Item_no,detail.Product_name,detail.Product_code)
+        // item_master_id:=<-item_master_id_chan
+        ////////////////////////////////////////
+        // uom_id:=get_uom_id(detail.Uom)
 
-            uom_id_chan :=make(chan string)
-            go get_uom_id_chan(uom_id_chan,detail.Uom)
-            uom_id:=<-uom_id_chan
-            item_master_id:=<-item_master_id_chan
-            if uom_id==""{
-            	return error_purchase_order_detail_uom_id,errors.New("purchase_order.detail uom_id is missed")
-            }
-            if item_master_id==""{
-            	return error_purchase_order_detail_item_master_id,errors.New("purchase_order.detail item_master_id is missed")
-            }
+        uom_id_chan :=make(chan string)
+        go get_uom_id_chan(uom_id_chan,detail.Uom)
+        uom_id:=<-uom_id_chan
+        item_master_id:=<-item_master_id_chan
+        if uom_id==""{
+        	return error_purchase_order_detail_uom_id,errors.New("purchase_order.detail uom_id is missed")
+        }
+        if item_master_id==""{
+        	return error_purchase_order_detail_item_master_id,errors.New("purchase_order.detail item_master_id is missed")
+        }
             
 		_, err = db.Exec(
         `INSERT INTO t_purchase_order_detail(detail_id,purchase_order_id,
