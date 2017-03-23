f9def73a832989a334f2f754681fb24f7a5c9b1c
lzxm160 <lzxm160@sina.com>
2017-03-23 17:54:39 +0800

    new json

--- src/mainproject/test_mysql_time.go
+++ src/mainproject/test_mysql_time.go
@@ -2,9 +2,9 @@
  import (
     "logger"
     "fmt"
-    // "encoding/json"
+    "encoding/json"
     "net/http"
-    // "io/ioutil"
+    "io/ioutil"
     // "bytes"
     // "time"
     // "errors"
@@ -22,6 +22,16 @@ func test_mysql_time (w http.ResponseWriter, r *http.Request) {
 	if r.Method !="POST"{
 		fmt.Fprint(w, "this interface should be post!")
 	} else{
+		body, _:= ioutil.ReadAll(r.Body)
+	    
+	    // log.Println(string(body))
+	    var t PoData  
+	    // var po_shared_data shared_data
+	    err_decode := json.Unmarshal(body, &t)
+	    // bytes.Trim(body,"\\r\\n")
+	    // line := strings.Trim(string(body), "\r\n")
+		defer r.Body.Close()
+		
 		ret:=single_select()
 	    fmt.Fprint(w,ret )
 	    log_str:=fmt.Sprintf("Started %s %s for %s:%s response:%s", r.Method, r.URL.Path, addr,"body",ret)
