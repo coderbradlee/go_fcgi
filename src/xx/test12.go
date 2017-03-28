44f234b78dd3a3713406c85a335395324ea7315b
lzxm160 <lzxm160@sina.com>
2017-03-28 15:07:41 +0800

    response code change to int

--- src/xx/test12.go
+++ src/xx/test12.go
@@ -10,12 +10,12 @@ import (
 	"sync"
 	// "testing"
 	// "math"
-	"reflect"
+	// "reflect"
 	// "unsafe"
 	// "os"
 	// "runtime/pprof"
 	// "time"
-	"encoding/json"
+	// "encoding/json"
 )
 type Data struct{
 	num int
@@ -45,23 +45,9 @@ func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 func main() {
-	var str string = "one"
-    var in interface{} = "one"
-    fmt.Println("str == in:",str == in,reflect.DeepEqual(str, in)) 
-    //prints: str == in: true true
-    v1 := []string{"one","two"}
-    v2 := []interface{}{"one","two"}
-    fmt.Println("v1 == v2:",reflect.DeepEqual(v1, v2)) 
-    //prints: v1 == v2: false (not ok)
-    data := map[string]interface{}{
-        "code": 200,
-        "value": []string{"one","two"},
-    }
-    encoded, _ := json.Marshal(data)
-    var decoded map[string]interface{}
-    json.Unmarshal(encoded, &decoded)
-    fmt.Println("data == decoded:",reflect.DeepEqual(data, decoded)) 
-    fmt.Println(data)
-    fmt.Println(decoded)
+	defer func () {
+		fmt.Println("recovered:",recover())
+	}()
+	panic("fail")
 }
 
