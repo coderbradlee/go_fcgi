e76fedd92a69863e752d7b31191c2cf5280f71da
lzxm160 <lzxm160@sina.com>
2017-03-28 16:03:00 +0800

    test

--- src/xx/test12.go
+++ src/xx/test12.go
@@ -57,7 +57,7 @@ func main() {
 	dir2:=path[sep+1:]
 	fmt.Println("dir1:",string(dir1))
 	fmt.Println("dir2:",string(dir2))
-	dir=append(dir1,"suffix"...)
+	dir1=append(dir1,"suffix"...)
 	path=bytes.Join([][]byte{dir1,dir2},[]byte{'/'})
 	fmt.Println("dir1:",string(dir1))
 	fmt.Println("dir2:",string(dir2))
