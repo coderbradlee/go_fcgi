89db06ab1bc7260be53143f575713ea8157561b6
lzxm160 <lzxm160@sina.com>
2017-03-28 14:52:20 +0800

    response code change to int

--- src/xx/test12.go
+++ src/xx/test12.go
@@ -44,8 +44,21 @@ func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 func main() {
-	m1:=map[string]string{"one":"1","two":"2"}
-	m2:=map[string]string{"one":"1","two":"2"}
-	fmt.Println("m1==m2:",reflect.DeepEqual(m1,m2))
+	var str string = "one"
+    var in interface{} = "one"
+    fmt.Println("str == in:",str == in,reflect.DeepEqual(str, in)) 
+    //prints: str == in: true true
+    v1 := []string{"one","two"}
+    v2 := []interface{}{"one","two"}
+    fmt.Println("v1 == v2:",reflect.DeepEqual(v1, v2)) 
+    //prints: v1 == v2: false (not ok)
+    data := map[string]interface{}{
+        "code": 200,
+        "value": []string{"one","two"},
+    }
+    encoded, _ := json.Marshal(data)
+    var decoded map[string]interface{}
+    json.Unmarshal(encoded, &decoded)
+    fmt.Println("data == decoded:",reflect.DeepEqual(data, decoded)) 
 }
 
