5728dc7780fa480f2302288f0e9ff30fd16de921
lzxm160 <lzxm160@sina.com>
2017-03-29 14:09:58 +0800

    test

--- src/xx/test12.go
+++ src/xx/test12.go
@@ -18,56 +18,49 @@ import (
 	// "encoding/json"
 	// "bytes"
 )
-type Data struct{
-	num int
-	key *string
-	items map[string]bool
-}
-func (this *Data)pmethod() {
-	this.num=7
-}
-func (this Data)vmethod() {
-	this.num=8
-	*this.key="v.key"
-	this.items["vmethod"]=true
-}
+
 var lock sync.Mutex
-func test(){
-	lock.Lock()
-	lock.Unlock()
-}
-func test_defer() {
-	lock.Lock()
-	defer lock.Unlock()
-}
 
 func init() {
 	// runtime.NumCPU()
 	runtime.GOMAXPROCS(runtime.NumCPU())
 }
-func get() []byte {  
-    raw := make([]byte,10000)
-    fmt.Println(len(raw),cap(raw),&raw[0]) //prints: 10000 10000 <byte_addr_x>
-    return raw[:3]
+func bind(f func(in io.Reader,out io.Writer,p []string),params []string)func(in io.Reader,out io.Writer) {
+	return func(in io.Reader,out io.Writer) {
+		f(in,out,params)
+	}
 }
-type myLocker struct{
-	sync.Mutex
-}
-type field struct{
-	name string
-}
-type data struct {  
-    name string
+func pipe(app1 func(in io.Reader,out io.Writer),app2 func(in io.Reader,out io.Writer))func(in io.Reader,out io.Writer) {
+	return func(in io.Reader,out io.Writer) {
+		r,w:=os.Pipe()
+		defer w.Close()
+		app1(in,w)
+		go func() {
+			defer r.Close()
+			app2(r,out)
+		}
+	}
 }
 func main() {
-	done:=false
-	go func () {
-		done=true
-	}()
-	for !done{
-		fmt.Println("xx")
-		runtime.Gosched()
+	f:=func(in io.Reader,out io.Writer,params []string) {
+		var command string
+		if b, err := ioutil.ReadAll(in); err == nil {
+		    command=string(b)
+		}
+		cmd := exec.Command(command, params)
+		cmd.Stdout =out
+		err = cmd.Start()
+		if err != nil {
+			fmt.Println("cmd error!")
+		}
 	}
+	fp:=bind(f,"note")
+	// fp2:=bind(f,"select")
+	fp(strings.NewReader("cat"),os.Stdout)
+
+	// pp:=pipe(fp,fp2)
+	// pp(strings.NewReader("cat"),os.Stdout)//cat note|grep select
+	                      //
 	fmt.Println("done!")
 }
 
