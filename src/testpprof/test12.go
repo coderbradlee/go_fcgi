86c5013e8a528ed21e9df55a1b2c9e973a5b3382
lzxm160 <lzxm160@sina.com>
2017-04-14 17:34:09 +0800

    testpprof

--- src/testpprof/test12.go
+++ src/testpprof/test12.go
@@ -30,6 +30,17 @@ func fib(n int)int {
 	}
 	return fib(n-1)+fib(n-2)
 }
+const m1 = 0x5555555555555555
+const m2 = 0x3333333333333333
+const m4 = 0x0f0f0f0f0f0f0f0f
+const h01 = 0x0101010101010101
+func popcnt(x uint64) uint64 {
+	x -= (x >> 1) & m1
+	x = (x & m2) + ((x >> 2) & m2)
+	x = (x + (x >> 4)) & m4
+	return (x * h01) >> 56
+}
+
 func main() {
 	// cpu, _ := os.Create("cpu.out")
 	// defer cpu.Close()
--- src/testpprof/test12_test.go
+++ src/testpprof/test12_test.go
@@ -2,7 +2,16 @@ package main
 import (
 	"testing"
 )
-func BenckmarkFib(b *test.B) {
+func BenchmarkPopcnt(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		x := i
+		x -= (x >> 1) & m1
+		x = (x & m2) + ((x >> 2) & m2)
+		x = (x + (x >> 4)) & m4
+		_ = (x * h01) >> 56
+	}
+}
+func BenckmarkFib(b *testing.B) {
 	for n:=0;n<b.N;n++{
 		fib(20)
 	}
